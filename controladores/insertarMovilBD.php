2c90e4eeb112116112e370a2a6fcf4a96f8f45fb
jerech <jeremiaschaparro@yahoo.com.ar>
2014-11-16 19:31:00 -0300

    Se implementa ABM Moviles

---
+++ controladores/insertarMovilBD.php
@@ -0,0 +1,28 @@
+<?php
+	include("/BD/conexionBD.php");
+	
+	$numero=$_POST["numero"];
+	$patente=$_POST["patente"];
+	$vencseguro=$_POST["vencseguro"];
+	$marca=$_POST["marca"];
+	$modelo=$_POST["modelo"];
+	$aire=$_POST["aire"];
+	$gnc=$_POST["gnc"];
+	
+	//El script lo que hara es verificar que los campos del formulario tengan datos
+if(!empty($numero) && !empty($patente) && !empty($marca) && !empty($modelo)){ //Si los campos tienen valores
+   //Si todos los datos provenientes de los campos en el formulario
+   //tienen datos se proceden a insertar en base de datos
+   	$conectar = establecerConexion();
+   	$sql = "INSERT INTO Moviles(patente,numero,marca,modelo,vencseguro,aa,gnc) values('$patente','$numero','$marca','$modelo','$vencseguro','$aire','$gnc')";
+   	$insertar = mysql_query($sql,$conectar);
+	if($insertar){
+		echo "1"; //Enviamos el valor 1
+		mysql_close($conectar);//Cerramos la conexion
+	}else{
+		// echo mysqli_error($conectar);
+		 echo "0"; //Enviamos el valor 0
+		 mysql_close($conectar); //Cerramos la conexion
+	}
+}
+?>
