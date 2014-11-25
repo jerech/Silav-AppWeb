b5cdc8e35e73efe37d117e71054607d8f5642654
sebastianbe <seba_b72@hotmail.com>
2014-11-25 13:12:50 -0300

    Cambios en la interfaz de usuario principal

---
+++ controladores/mapa.js
@@ -0,0 +1,12 @@
+function init() {
+	map = new OpenLayers.Map("contenedorMapa");
+   var mapnik = new OpenLayers.Layer.OSM();
+   map.addLayer(mapnik);
+   var lonlat = new OpenLayers.LonLat(-62.086, -31.430).transform(
+   	new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984		new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator	);
+   var zoom = 15;
+   var markers = new OpenLayers.Layer.Markers( "Markers" );
+   map.addLayer(markers);
+   markers.addMarker(new OpenLayers.Marker(lonlat));
+   map.setCenter(lonlat, zoom);
+}
