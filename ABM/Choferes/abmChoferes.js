d2c6fc7df988432cfa99cadcbbb34021609cd31d
jerech <jeremiaschaparro@yahoo.com.ar>
2014-11-25 22:52:43 -0300

    Se termina con ABM moviles y choferes

--- ABM/Choferes/abmChoferes.js
+++ ABM/Choferes/abmChoferes.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-	$("#tabla").load("tablaChoferes.php");
+	$("#div-tabla").load("tablaChoferes.php");
+	$("#tabla-moviles").load('tablaEdicionMoviles.php');
 	
 		$("#guardar").click(function () {
 		var datosform = $("#formulario").serialize();
@@ -15,7 +16,7 @@ $(document).ready(function () {
 		   success: function(datos){ //Funcion que retorna los datos procesados del script PHP
 			  if(datos == 1){ //Dato que proviene del script php
 				 $("#estado").html("Procesado satisfactoriamente"); //Mensaje de Satisfacción
-				 $("#tabla").load('tablaMoviles.php');
+				 $("#div-tabla").load('tablaChoferes.php');
 			  }else if(datos == 0){ //Dato que sproviene del script php
 				 $("#estado").html("Por favor ingrese todos los datos"); //Mensaje de error
 			  }
@@ -24,10 +25,92 @@ $(document).ready(function () {
 	
 	
 	});
+	
+	$("#cerrar").click(function () {
+		setear_moviles_seleccionados();
+		$('.overlay-container').fadeOut().end().find('.window-container').removeClass('window-container-visible');
+		
+	
+	});
 });
 
+function setear_moviles_seleccionados() {
+	var numMovil="", valCheckbox="";
+	var movilesCheck="";
+	var flagPrimerMovil=true;
 
-	function fnc_eliminar(id){
+	$('#tabla-edit-moviles tbody tr').each(function (index) {
+		$(this).children("td").each(function (index2) {
+			if(index2==0){
+				numMovil=$(this).text();			
+			}
+			if (index2==4) {
+				valCheckbox = $(this).find('.check-edit-moviles').is(':checked');
+				if (valCheckbox==true) {
+					if(!flagPrimerMovil){		
+						movilesCheck=movilesCheck+"-"+numMovil;
+					}else {
+						movilesCheck=numMovil;
+						flagPrimerMovil=false;
+					}
+				}
+			}
+		
+		});
+		
+	});
+	$("#listamoviles").attr("value", movilesCheck);
+}
+
+function mostrar_tabla_moviles() {
+	
+	var moviles = $("#listamoviles").attr("value");
+	var listamoviles="";
+	try {
+		listamoviles=moviles.split("-");
+		checker_moviles_tabla(listamoviles);
+	}catch (error) {
+		
+	}
+	$('.overlay-container').fadeIn(function () {
+		$('.window-container').addClass('window-container-visible');
+	});
+	
+}
+
+function checker_moviles_tabla(listamoviles) {
+	var numMovil="";
+	var hacerCheck=false;
+
+	$('#tabla-edit-moviles tbody tr').each(function (index) {
+		$(this).children("td").each(function (index2) {
+			if(index2==0){
+				numMovil=$(this).text();		
+				for (var i=0; i<listamoviles.length; i++) {
+					if(numMovil==listamoviles[i]){
+						hacerCheck=true;					
+					}
+				}	
+			}
+
+			if (index2==4) {				
+				if(hacerCheck){
+					$(this).find('.check-edit-moviles').prot("checked",true);
+				}else {
+					$(this).find('.check-edit-moviles').prot("checked",false);
+				}
+	
+			}
+		
+		});
+		hacerCheck=false;
+		
+	});
+	
+}
+
+
+function fnc_eliminar(id){
 	  $.ajax({
 		   type: "POST", //Establecemos como se van a enviar los datos puede POST o GET
 		   url: "eliminarChoferBD.php", //SCRIPT que procesara los datos, establecer ruta relativa o absoluta
@@ -39,7 +122,7 @@ $(document).ready(function () {
 		   dataType: "html",
 		   success: function(datos){ //Funcion que retorna los datos procesados del script PHP
 			  if(datos == 1){ //Dato que proviene del script php
-				  $("#tabla").load('tablaChoferes.php');
+				  $("#div-tabla").load('tablaChoferes.php');
 				  $("#estado").html("Proceso realizado correctamente (Eliminar)"); //Mensaje de error
 			  }else if(datos == 0){ //Dato que proviene del script php
 				 $("#estado").html("Error al procesar script, verifique sus datos"); //Mensaje de error
@@ -47,3 +130,42 @@ $(document).ready(function () {
 		   }
 	   });
 	}
+	
+function fnc_editar(id) {
+		$.ajax({
+			type: "POST",
+			url: "consultaChoferBD.php",
+			data: 'id=' + id,
+			contentType: "application/x-www-form-urlencoded", 
+			beforeSend: function() {//Función que se ejecuta antes de enviar los datos
+			  $("#estado").html("Procesando...."); //Mostrar mensaje que se esta procesando el script
+		   },
+		   dataType: "html",
+		   success: function(datos){ //Funcion que retorna los datos procesados del script PHP
+		   
+		   var parametros = datos.split("&&");
+		 
+		  	$("#nombre").attr("value",parametros[0]);  	
+		  	$("#apellido").attr("value",parametros[1]);
+		  	$("#usuario").attr("value",parametros[2]);
+		  	$("#usuario").attr("readonly",true);
+		  	$("#pass").attr("value",parametros[3]);
+		  	$("#telefono").attr("value",parametros[4]);
+		  	$("#venclicencia").attr("value",parametros[5]);
+		  	$("#listamoviles").attr("value",parametros[8]);
+		  	
+		  	if (parametros[6]=="si") {
+		  		$("#habilitado").attr("checked",true);
+		  	}
+		  	if (parametros[7]=="mujer") {
+		  		$("#sexo_m").attr("checked",true);
+		  	}else {
+		  		$("#sexo_m").attr("checked",false);
+		  		}
+		  	 		
+			$("#estado").html("Movil listo para editar"); 
+			  
+		   }
+						
+			});
+		}
---
+++ ABM/Choferes/tablaEdicionMoviles.php~
@@ -0,0 +1,29 @@
+<?php
+	include("../conexionBD.php");
+	echo "<table class='tabla-mostrar-moviles' id=tabla-edit-moviles>";
+    echo "<tr>";
+    echo "<td>Num. Movil</td><td>Patente</td><td>Marca</td><td>Modelo</td>";
+    echo "</tr>";
+	$color = 0;
+	$conectar = establecerConexion();
+    $sql = "SELECT numero,patente,marca,modelo FROM Moviles order by numero";
+    $select = mysql_query($sql,$conectar);
+	if($select){
+		while($fila = mysql_fetch_array($select)){
+?>
+				<tr>
+				<td><?=$fila[0]?></td>
+				<td><?=$fila[1]?></td>
+				<td><?=$fila[2]?></td>
+				<td><?=$fila[3]?></td>
+				<td><input class="check-edit-moviles" type="checkbox"/></td>
+				</tr>
+<?php
+
+		}
+		mysql_close($conectar);//Cerramos la conexion
+	}else{
+		 mysql_close($conectar); //Cerramos la conexion
+	}
+	echo "</table>";
+?>
