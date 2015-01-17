52c05f78e36e4c8893b30a122bc7331fa766ac16
sebastianbe <seba_b72@hotmail.com>
2015-01-17 16:18:03 -0300

    se agregan archivos

--- BD/silav.sql
+++ BD/silav.sql
@@ -144,44 +144,6 @@ CREATE TABLE IF NOT EXISTS `Pasajes` (
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `Permisos`
---
-
-CREATE TABLE IF NOT EXISTS `Permisos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `Usuarios_id` int(11) NOT NULL,
-  `Secciones_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`Usuarios_id`,`Secciones_id`),
-  KEY `fk_Permisos_Usuarios_idx` (`Usuarios_id`),
-  KEY `fk_Permisos_Secciones1_idx` (`Secciones_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=8 ;
-
---
--- Dumping data for table `Permisos`
---
-
-REPLACE INTO `Permisos` (`id`, `Usuarios_id`, `Secciones_id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 1, 4),
-(4, 1, 5),
-(5, 1, 6),
-(6, 1, 3),
-(7, 1, 50);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Secciones`
---
-
-CREATE TABLE IF NOT EXISTS `Secciones` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) COLLATE latin1_spanish_ci NOT NULL,
-  `id_seccion_padre` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=62 ;
 
 --
 -- Dumping data for table `Secciones`
@@ -242,15 +204,22 @@ REPLACE INTO `Usuarios` (`id`, `nombre`, `apellido`, `usuario`, `contrasenia`, `
 --
 
 --
--- Constraints for table `Permisos`
---
-ALTER TABLE `Permisos`
-  ADD CONSTRAINT `fk_Permisos_Secciones1` FOREIGN KEY (`Secciones_id`) REFERENCES `Secciones` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Permisos_Usuarios` FOREIGN KEY (`Usuarios_id`) REFERENCES `Usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-
+
+
+-- --------------------------------------------------------
+
+--
+-- Constraints for dumped tables
+--
+--
+-- Dumping data for table `Permisos`
+--
+
 --
 -- Table structure for table `Permisos`
 --
@@ -264,65 +233,11 @@ CREATE TABLE IF NOT EXISTS `Permisos` (
   KEY `fk_Permisos_Secciones1_idx` (`Secciones_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=8 ;
 
-
-
-CREATE TABLE IF NOT EXISTS `Secciones` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) COLLATE latin1_spanish_ci NOT NULL,
-  `id_seccion_padre` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=51 ;
-
---
--- Dumping data for table `Secciones`
---
-
-INSERT INTO `Secciones` (`id`, `nombre`, `id_seccion_padre`) VALUES
-(1, 'Inicio', NULL),
-(2, 'Administradores', NULL),
-(3, 'Operadores', NULL),
-(4, 'Moviles', NULL),
-(5, 'Choferes', NULL),
-(6, 'Geolocalizacion', NULL),
-(50, 'NuevoAdministrador', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `Usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `apellido` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `usuario` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `contrasenia` varchar(70) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `telefono` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `id_agencia` int(11) NOT NULL,
-  `email` varchar(45) COLLATE latin1_spanish_ci DEFAULT NULL,
-  `tipo` varchar(45) COLLATE latin1_spanish_ci NOT NULL,
-  `activo` tinyint(1) DEFAULT NULL,
-  `direccion` varchar(45) COLLATE latin1_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `usuario` (`usuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `Usuarios`
---
-
-INSERT INTO `Usuarios` (`id`, `nombre`, `apellido`, `usuario`, `contrasenia`, `telefono`, `id_agencia`, `email`, `tipo`, `activo`, `direccion`) VALUES
-(1, 'jere', 'chaparro', 'jere', '845838210a77ea38b9a662d6f28fd7f3', NULL, 0, NULL, 'admin', 1, NULL);
-
---
--- Constraints for dumped tables
---
 --
 -- Dumping data for table `Permisos`
 --
 
-INSERT INTO `Permisos` (`id`, `Usuarios_id`, `Secciones_id`) VALUES
+REPLACE INTO `Permisos` (`id`, `Usuarios_id`, `Secciones_id`) VALUES
 (1, 1, 1),
 (2, 1, 2),
 (3, 1, 4),
@@ -333,15 +248,13 @@ INSERT INTO `Permisos` (`id`, `Usuarios_id`, `Secciones_id`) VALUES
 
 -- --------------------------------------------------------
 
+
 --
 -- Table structure for table `Secciones`
 --
 --
 -- Constraints for table `Permisos`
 --
-ALTER TABLE `Permisos`
-  ADD CONSTRAINT `fk_Permisos_Secciones1` FOREIGN KEY (`Secciones_id`) REFERENCES `Secciones` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Permisos_Usuarios` FOREIGN KEY (`Usuarios_id`) REFERENCES `Usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
