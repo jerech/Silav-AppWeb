ba1bcb0e7af88a45704cf9d65167e1252d46ad0f
jeremias <jeremiaschaparro@gmail.com>
2015-01-27 12:29:16 -0300

    Se termina de hacer la funcionalidad de modificar administrador

--- controladores/administradores/modificar.php
+++ controladores/administradores/modificar.php
@@ -6,9 +6,10 @@
     }
 
 	//Se verifica que los campos obligatorios no esten vacios
-  	if(empty($_POST['contrasenia-encriptada'])||empty($_POST['nombre'])||empty($_POST['apellido']) ||empty($_POST['usuario'])){
+  	if(empty($_POST['nombre'])||empty($_POST['apellido']) ||empty($_POST['usuario'])){
   		echo "Error. Campos obligatorios vacios.";
   		exit();
+
   	}
 
     if($_POST['contrasenia-encriptada'] != $_POST['re-contrasenia-encriptada']){
@@ -19,6 +20,7 @@
   	require_once("../../conexionBD.php");
 
   	//Se obtienen los datos a guardar
+    $id = $_POST['id'];
   	$nombre = $_POST['nombre'];
   	$apellido = $_POST['apellido'];
   	$usuario = $_POST['usuario'];
@@ -35,28 +37,19 @@
   	}
 
   	//Se realiza el insert en la BD
-
   	$query = "
-  			insert into Usuarios(
-  				nombre,
-  				apellido,
-  				usuario,
-  				contrasenia,
-  				telefono,
-  				direccion,
-  				tipo,
-  				email,
-  				activo)
-  			values(
-  				'$nombre',
-  				'$apellido',
-  				'$usuario',
-  				'$contrasenia',
-  				'$telefono',
-  				'$direccion',
-  				'$tipo',
-  				'$email',
-  				'$activo')";
+  			UPDATE  
+              Usuarios
+        SET
+  				    nombre='$nombre',
+  				    apellido='$apellido',
+  				    usuario='$usuario',
+  				    telefono='$telefono',
+  				    direccion='$direccion',
+  				    email='$email',
+  				    activo='$activo'
+        WHERE 
+              id=$id";
 
 	$conexion = establecerConexion();
 	if(!$conexion){
@@ -67,15 +60,25 @@
 	$resultado = mysql_query($query, $conexion) or die('Error: '.mysql_error().'. Nro: '.mysql_errno());
 
 	if($resultado){
-		guardarPermisos(mysql_insert_id(), $permisos);
-		echo "OK_".mysql_insert_id(); //Con la funcion mysql_insert_id() se obtiene el id del elemento insertado
+		guardarPermisos($id, $permisos);
+		echo "OK_"; //Con la funcion mysql_insert_id() se obtiene el id del elemento insertado
 	}
+
+  if(!empty($_POST['contrasenia-encriptada']) && $_POST['contrasenia-encriptada'] != " "){
+    $contrasenia = $_POST['contrasenia-encriptada'];
+    modificarContrasenia($id, $contrasenia);
+  }
 
 	mysql_close($conexion);
 
 
 	function guardarPermisos($idUsuario, $permisos){
 		if(!empty($permisos)){
+      $queryBorrarPermisos = "DELETE FROM 
+                                Permisos
+                              WHERE
+                                Usuarios_id = $idUsuario";
+      mysql_query($queryBorrarPermisos);
                     foreach ($permisos as $permiso) {
                         $idSeccion = $permiso;
                         /*
@@ -89,11 +92,21 @@
                                         $idUsuario,
                                         $idSeccion)";
 
-                        $respuesta = mysql_query($query)or die('Error: '.mysql_error().'. Nro: '.mysql_errno());;
+                        $respuesta = mysql_query($query)or die('Error: '.mysql_error().'. Nro: '.mysql_errno());
                         
                      		
                     }	
                 }
 
-	} 
+	}
+
+  function modificarContrasenia($id, $contrasenia){
+    $query = "UPDATE
+                Usuarios
+              SET
+                contrasenia='$contrasenia'
+              WHERE
+                id=$id";
+    mysql_query($query)or die('Error: '.mysql_error().'. Nro: '.mysql_errno());
+  } 
 ?>
