e3d8c8cf75ba53c726fa401a53a9b5936fe400a8
jerech <jeremiaschaparro@yahoo.com.ar>
2015-06-27 18:29:44 -0300

    Se agrega vista de Seccion no encontrada y acceso no autorizado

---
+++ vistas/accesoNoAutorizado.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+	<div class="content">
+<h1><b>Acceso no autarizado!<br></h1>
+<h3>Comuniquese con el administrador.</h3>
+</div>
+
+</body>
+
+
+</html>
---
+++ vistas/seccionNoEncontrada.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+	<div class="content">
+<h1><b>Sección no encontrada!<br></h1>
+<h3>Comuniquese con el administrador.</h3>
+</div>
+
+</body>
+
+
+</html>
